a5ca49062c102a4c37c9c5ca6259a74164a02250
shrieghimire <shredde111@gmail.com>
2022-06-25 19:01:38 +0545

    Net CC lab 2

---
+++ ECB/ECB/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECB.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Signup()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+        public IActionResult AddDish()
+        {
+            return View();
+        }
+        public IActionResult AddRecipe()
+        {
+            return View();
+        }
+    }
+}
---
+++ ECB/ECB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ECB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
